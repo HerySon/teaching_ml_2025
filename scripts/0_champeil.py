4a21347a5f4ef56a0851c519184ed83910ce5bc2
SULFYY <70634014+ChTheo@users.noreply.github.com>
2025-02-24 15:39:07 +0100

    Separated code in multiple functions

--- scripts/0_champeil.py
+++ scripts/0_champeil.py
@@ -4,27 +4,39 @@ Will also reduce the size of floats to increase performances
 Args : df -> the dataframe to be modified
 Return : df -> the modified dataframe
 """
+import pandas as pd
+import numpy as np
+def get_types(df) :
 
-def filter_df(df) :
+    return df.dtypes
 
-    # Check the columns type
-
-    df.dtypes
-
-    # Display numerical columns
+def get_numerical(df):
+    # store numerical columns in an array
     numeric_columns = df.select_dtypes(include=[np.number]).columns
-    print(numeric_cols)
+    return numeric_columns
 
-    # Display categorical columns -> we saw with df.dtypes that non numerical columns are "object"
-
+def get_categorical(df):
+    # store categorical columns in an array -> we saw with df.dtypes that non numerical columns are "object"
     categorical_columns = df.select_dtypes(include=[object]).columns
-    print(categorical_columns)
+    return categorical_columns
 
+def downcast_floats(df):
     # Reduce float size to increase performance and reduce dataframe size
     # Target type can be ajusted from float32 to any that suits
     for col in df.select_dtypes(include=[np.float64]).columns:
         df[col] = df[col].astype(np.float32)
-
-    # No equivalent for int type in this project because there is no int type column besides the row number,code, and timestamps, which seems irrelevent for training
-
     return df
+
+'''
+#Playground for testing functions
+
+path="../data/dataset.csv"
+df = pd.read_csv(path)
+print(get_types(df))
+print(get_numerical(df))
+print(get_categorical(df))
+
+df=downcast_floats(df)
+print(get_types(df))
+
+'''
